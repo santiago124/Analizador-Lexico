68aae2671db7617adf2262fda0cb923f84748909
jamesrx13 <rudasmarinjf@gmail.com>
2023-02-20 16:50:14 -0500

    Ya lee pero todo con espacios

--- js.js
+++ js.js
@@ -1,19 +1,43 @@
-document.addEventListener('DOMContentLoaded', () => {
+const library = {
+    'for': 'Es una palabra reservada para un bucle',
+    '(': 'Parentisis de apertura',
+    ')': 'Parentisis de cierre',
+    '+': 'Operador de suma',
+    '++': 'Incrementador en 1',
+    '{': 'Llave de apertura',
+    '}': 'Llave de cierre',
+    '=': 'Operador de asignación',
+    ';': 'Delimitador',
+    '<': 'Menor que',
+    '>': 'Mayor que',
+}
 
-    const library = {
-        'for': 'Es una palabra reservada para un bucle',
-        '(': 'Parentisis de apertura',
-        ')': 'Parentisis de cierre',
+
+const analyzer = () => {
+    const textArea = document.getElementById('texto').value.trim()
+    var temp = ''
+
+    for (let index = 0; index < textArea.length; index++) {
+        if (textArea[index] == ' ') {
+            temp = ''
+            continue
+        }
+
+        temp += textArea[index]
+
+        if (library[temp] != undefined) {
+            console.log(library[temp])
+            temp = ''
+        } else {
+            continue
+        }
+
     }
 
-    document.getElementById('btnText', analyzer)
+    console.log(temp)
+}
 
-    const analyzer = () => {
-        const textArea = document.getElementById('texto')
 
-    }
-
-}, false)
 
 
 /*
